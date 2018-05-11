30f08504fc1e75c7df50c6e558ac298fa7d5a82d
VishalBilagi <vishalb98@gmail.com>
2018-05-11 17:09:59 +0530

    collecting data into data frame and storing as csv

--- src/scrap.py
+++ src/scrap.py
@@ -12,48 +12,17 @@ import re
 #Everybody needs sleep :P even pyhtons
 from time import sleep
 
-#To fetch address bar url text from chrome's current tab
-#import uiautomation as automation
+import pandas as pd
 
-#Listen to keyboard hits for capturing url when pause/break key is pressed
-#from pynput import keyboard
+cols = ('Product-ID', 'Ratings', 'Review-Title', 'Review-Text', 'Helpful-Votes', 'Review-ID')
+df1 = pd.DataFrame(columns = cols)
 
-# def on_press(key):
-#     try:
-#         # print('alphanumeric key {0} pressed'.format(key.char))
-#         ...
-#     except AttributeError:
-#         # print('special key {0} pressed'.format(key))
-#         ...
-
-
-# def on_release(key):
-#     # print('{0} released'.format(key))
-#     if key == keyboard.Key.esc:
-#         # Stop listener
-#         exit(0)
-#     if key == keyboard.Key.pause:
-#         getReviewData()
-
+debug = False
 
 def getReviewData():
 	product = ""
-	# sleep(3)
-	# control = automation.GetFocusedControl()
-	# controlList = []
-	# while control:
-	# 	controlList.insert(0, control)
-	# 	control = control.GetParentControl()
-	# if len(controlList) == 1:
-	# 	control = controlList[0]
-	# else:
-	# 	control = controlList[1]
-    # #Look for chrome's current tab's address bar
-	# address_control = automation.FindControl(control, lambda c, d: isinstance(
-	#     c, automation.EditControl) and "Address and search bar" in c.Name)
-
-	#Load product's review url into product
-	product = sys.argv[1].replace('/dp/','/product-reviews/')# = address_control.CurrentValue().replace('/dp/', '/product-reviews/')
+	
+	product = sys.argv[1].replace('/dp/','/product-reviews/')
 	print(product)
 
 	#Open and parse the reviews site url
@@ -73,23 +42,32 @@ def getReviewData():
 	# 4) Review ID
 
 	for reviewContent, reviewVoteCount in zip(reviewBody, reviewVotes):
-		print("Ratings: " +
-		      str(reviewContent.contents[0].contents[0].contents[0].contents[0].contents[0]))
-		print("Review Title: " +
-		      str(reviewContent.contents[0].contents[2].contents[0]))
-		pattern = re.compile(r', <br\/>,|\[|\]')
-		print("Review Text: " + pattern.sub('',
-		      str(reviewContent.contents[3].contents[0].contents)))
+		pidPattern = re.compile(r'\/[A-Z0-9]{10}\/')
+		reviewPattern = re.compile(r', <br\/>,|\[|\]')
 		votePattern = re.compile(r'\n|[ a-zA-Z.]')
+		if(debug):
+			print("Product ID: "+ pidPattern.search(product).group().replace('/',''))
+			print("Ratings: " + str(reviewContent.contents[0].contents[0].contents[0].contents[0].contents[0]))
+			print("Review Title: " + str(reviewContent.contents[0].contents[2].contents[0]))
+			print("Review Text: " + reviewPattern.sub('',str(reviewContent.contents[3].contents[0].contents)))
+			re.compile(r'One person found this helpful').sub('1', str(reviewVoteCount.get_text()))
+			print("Helpful Votes: " + votePattern.sub('',str(reviewVoteCount.get_text())))
+			print("Review ID: " +str(reviewContent.get('id')).replace('customer_review-',''))
+			print("")
+		cols = ('Product-ID', 'Ratings', 'Review-Title', 'Review-Text', 'Helpful-Votes', 'Review-ID')
+		lst=[]
+		pid = pidPattern.search(product).group().replace('/','')
+		ratings = str(reviewContent.contents[0].contents[0].contents[0].contents[0].contents[0])
+		ratings = ratings[0]
+		title = str(reviewContent.contents[0].contents[2].contents[0])
+		text = reviewPattern.sub('',str(reviewContent.contents[3].contents[0].contents))
 		re.compile(r'One person found this helpful').sub('1', str(reviewVoteCount.get_text()))
-		print("Helpful Votes: " + votePattern.sub('',str(reviewVoteCount.get_text())))
-		print("Review ID: " +str(reviewContent.get('id')).replace('customer_review-',''))
-		print("")
+		votes = votePattern.sub('',str(reviewVoteCount.get_text()))
+		rid = str(reviewContent.get('id')).replace('customer_review-','')
+		lst.append([pid,ratings,title,text,votes,rid])
+		df = pd.DataFrame(lst, columns = cols)
+		global df1
+		df1 = pd.concat([df1,df], ignore_index=True)
+		df1.to_csv("amazonReviews.csv", index=False)
+
 getReviewData()
-
-
-
-# with keyboard.Listener(
-#         on_press=on_press,
-#         on_release=on_release) as listener:
-#     listener.join()
