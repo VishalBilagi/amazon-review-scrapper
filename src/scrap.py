ebe4c4c2b9428d9dae71f7426bceba0b9eceaab3
VishalBilagi <vishalb98@gmail.com>
2018-06-11 11:50:03 +0530

    Changed scrap.py's review mining function to work in multithreading

--- src/scrap.py
+++ src/scrap.py
@@ -5,10 +5,13 @@ import urllib.request,urllib.error
 from bs4 import BeautifulSoup
 from dateparser import parse
 import sys
-from googledrive import sendCSV
-
+import http
 #For RegEX operations
 import re
+
+import math
+from multiprocessing.dummy import Pool as EpicPool
+from functools import partial
 
 #Everybody needs sleep :P even pyhtons
 from time import sleep
@@ -33,12 +36,17 @@ def openAndParse(success,product):
 						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
 					}
 				)
-				page = urllib.request.urlopen(req)
+				page = urllib.request.urlopen(req).read()
 				success = True
 				#print("opened")
-			except urllib.error.URLError as e: print(e.reason)
+			except urllib.error.URLError as e: 
+				print(e.reason)
+				success = False
 			#Wait some time before making another request
-			sleep(2)
+			except http.client.IncompleteRead as e: 
+				print('partial load')
+				success = False
+			#sleep(2)
 	if(success):
 		#Parse review page
 		return BeautifulSoup(page, 'html5lib')
@@ -46,53 +54,17 @@ def openAndParse(success,product):
 	else:
 		return -1
 
-def getReviewData():
-	product = ""
-	productURLPattern = re.compile(r'(\/gp\/product\/)|\/dp\/')
-	product = productURLPattern.sub('/product-reviews/',sys.argv[1])
-	matchProduct = re.search(r'product-reviews/.{11}',product)
-	product = product.replace(product[matchProduct.end():],'?pageNumber=1')
-	
-	#print(product)
-	success = False
-	soup = openAndParse(success,sys.argv[1])
-	### TO DO : 
-	# Inlcude Sales Rank 
-	# scrap appropriately for different types of products
-	### TO DO
-	#salesRank = soup.find(id="SalesRank")
-	#print( re.compile(r'[\n()]').sub('', str(salesRank.contents[1].contents[0])))
-	
-	try:
-		dateFirstAvaliable = parse(str(soup.find(class_="date-first-available").contents[1].contents[0])).strftime("%d/%m/%Y")
-	except: 
-		try: 
-			#print("This is a book")
-			dateFirstAvaliable = soup.find('b', text="Publisher:").next_sibling
-			dateFirstAvaliable = re.compile(r'[0-9]{1,2} [a-zA-Z]{1,10} [0-9]{1,4}').findall(str(dateFirstAvaliable))
-			dateFirstAvaliable = parse(dateFirstAvaliable[0]).strftime("%d/%m/%Y")
-			#print(dateFirstAvaliable)
-		except: dateFirstAvaliable = "NA"
+def getReviewData_mThreading(linksList,pdt,dfa):
+	df_list = []
+	product = pdt
+	dateFirstAvaliable = dfa
+	start = linksList[0]
 
-	
-	# print(dateFirstAvaliable)
-	success = False
-	soup = openAndParse(success,product)
+	while start <= linksList[1]:
+		matchPageNum = re.search(r'pageNumber=', product)
+		startPdt = product[:matchPageNum.end()] + str(start)
 
-	reviewPageCount = soup.find("div",{"id":"cm_cr-pagination_bar"})
-	if(reviewPageCount is not None):
-		if(len(reviewPageCount.contents[0]) <8):
-			reviewCount = reviewPageCount.contents[0].contents[len(reviewPageCount.contents[0])-2].contents[0].contents[0]
-		else:
-			reviewCount = reviewPageCount.contents[0].contents[6].contents[0].contents[0]
-	else:
-		reviewCount = 1
-	#print(reviewCount)
-	success = False
-	for x in range(0,int(reviewCount)):
-		print("Loading reviews "+str(x+1)+" of "+ str(reviewCount))
-
-		soup = openAndParse(success,product)
+		soup = openAndParse(False,startPdt)
 		#'a-section-cell-widget' class contains the review body
 		#There are total of ten reviews per page
 		reviewBody = soup.find_all(class_=re.compile("a-section celwidget"))
@@ -143,15 +115,108 @@ def getReviewData():
 			rDate = str(parse(date.text.replace('on ','')).strftime("%d/%m/%Y"))
 			lst.append([pid,dateFirstAvaliable,ratings,title,text,votes,rid,rDate])
 			df = pd.DataFrame(lst, columns = cols)
+			df_list.append(df)
+		start +=1
+	return df_list
+	
+
+
+def getReviewData():
+	product = ""
+	productURLPattern = re.compile(r'(\/gp\/product\/)|\/dp\/')
+	product = productURLPattern.sub('/product-reviews/',sys.argv[1])
+	matchProduct = re.search(r'product-reviews/.{11}',product)
+	product = product.replace(product[matchProduct.end():],'?pageNumber=1')
+	
+	#print(product)
+	success = False
+	soup = openAndParse(success,sys.argv[1])
+	### TO DO : 
+	# Inlcude Sales Rank 
+	# scrap appropriately for different types of products
+	### TO DO
+	#salesRank = soup.find(id="SalesRank")
+	#print( re.compile(r'[\n()]').sub('', str(salesRank.contents[1].contents[0])))
+	
+	try:
+		dateFirstAvaliable = parse(str(soup.find(class_="date-first-available").contents[1].contents[0])).strftime("%d/%m/%Y")
+	except: 
+		try: 
+			#print("This is a book")
+			dateFirstAvaliable = soup.find('b', text="Publisher:").next_sibling
+			dateFirstAvaliable = re.compile(r'[0-9]{1,2} [a-zA-Z]{1,10} [0-9]{1,4}').findall(str(dateFirstAvaliable))
+			dateFirstAvaliable = parse(dateFirstAvaliable[0]).strftime("%d/%m/%Y")
+			#print(dateFirstAvaliable)
+		except: dateFirstAvaliable = "NA"
+
+	
+	# print(dateFirstAvaliable)
+	success = False
+	soup = openAndParse(success,product)
+
+	reviewPageCount = soup.find("div",{"id":"cm_cr-pagination_bar"})
+	if(reviewPageCount is not None):
+		if(len(reviewPageCount.contents[0]) <8):
+			reviewCount = reviewPageCount.contents[0].contents[len(reviewPageCount.contents[0])-2].contents[0].contents[0]
+		else:
+			reviewCount = reviewPageCount.contents[0].contents[6].contents[0].contents[0]
+	else:
+		reviewCount = '1'
+	success = False
+	reviewCount = int(reviewCount.replace(',',''))
+	#print(reviewCount)
+	
+	#Load review pages as ranges in startList and endList arrays
+	#A range value (start in startList, end in endList) is taken to load reviews between page number [start] to page number [end]
+	#Total review pages (reviewCount) is divided by 10 to get 10 equal (except maybe the last set) range sets
+	startList = []
+	endList = []
+	x = math.ceil(reviewCount/10)
+	startNUM = 1
+	endNUM = x
+	totalNUM = 0
+	multiplier = 2
+	#iterate and load startNUM and endNUM into startList and endList respectively - for (start, end) pairs
+	while(totalNUM<reviewCount):
+		startList.append(startNUM)
+		endList.append(endNUM)
+		totalNUM += x
+		startNUM = endNUM + 1
+		#check if the page number doesn't exceed actual page numbers avaliable (reviewCount)
+		if multiplier*x < reviewCount:
+			endNUM = multiplier*x
+			multiplier += 1
+		else:
+			break
+	
+	#coumpute remainder pages start,end pair
+	lastNUM = reviewCount - endNUM
+	startNUM = endNUM+1
+	endNUM = startNUM + lastNUM - 1
+	startList.append(startNUM)
+	endList.append(endNUM)
+	
+	#generate range pairs [start,end]
+	linksList = []
+	for s,e in zip(startList,endList):
+		linksList.append([s,e])
+	
+	#setup multi threading
+	pool = EpicPool(10)
+	#load constant paramenters to be passed to getReviewData_mThreading
+	partial_fn = partial(getReviewData_mThreading, pdt=product, dfa = dateFirstAvaliable)
+	#df_set will hold a list of Data Frames computed from getReviewData_mThreading
+	df_set = pool.map(partial_fn, linksList)
+	
+	#extract Data Frames from df_list and append to df1 (main) Data Frame
+	for eachDf in df_set:
+		for x in eachDf:
 			global df1
-			df1 = pd.concat([df1,df], ignore_index=True)
-		global pageNum
-		matchPageNum = re.search(r'pageNumber=', product)
-		product = product[:matchPageNum.end()] + str(pageNum)
-		pageNum = pageNum+1
-		success = False
-		
-	df1.to_csv(pid+".csv", index=False)
-	sendCSV(pid)
+			df1 = pd.concat([df1,x], ignore_index=True)
+
+	#get pid value to save the Data Frame as <pid>.csv
+	pidPattern = re.compile(r'\/[A-Z0-9]{10}\/')
+	pid = pidPattern.search(product).group().replace('/','')
+	df1.to_csv(pid +".csv", index=False)
 
 getReviewData()
