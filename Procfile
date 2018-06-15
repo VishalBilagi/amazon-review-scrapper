d43c9e0e21a6e12bbf8e69b4e8c5b7e45fd50ce8
VishalBilagi <vishalb98@gmail.com>
2018-06-15 11:36:31 +0530

    Updated the server to work on Flask instead of NodeJS

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: node index.js
+web: python run.py
 pipinstall: pip install -r requirements.txt
---
+++ src/views.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, session, redirect, url_for, render_template, flash
+from .scrap import *
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    url = str(request.form['amazonURL'])
+    print('Incoming link: ' + url)
+    getReviewData(url)
+    return render_template('index.html')
