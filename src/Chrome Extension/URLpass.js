32b2b0cc5a182b00ce18fef7c51e230cd027abfa
VishalBilagi <vishalb98@gmail.com>
2018-12-26 10:56:35 +0530

    Code clean up

--- src/Chrome Extension/URLpass.js
+++ src/Chrome Extension/URLpass.js
@@ -1,8 +1,3 @@
-chrome.runtime.onInstalled.addListener(function(){
-    chrome.storage.sync.set({color: '#ffffff'}, function(){
-    })
-})
-
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
--- src/Chrome Extension/popup.js
+++ src/Chrome Extension/popup.js
@@ -2,17 +2,16 @@ let button = document.getElementById('btn');
 
 var url ;
 button.onclick = function(element) {
-    let color = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       url = tabs[0].url;
       chrome.tabs.executeScript(
           tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '"; chrome.storage.sync.set({\'URL\':"'+url+'"},function(){ console.log("SET: "+"'+url+'");});'});
+          {code: 'chrome.storage.sync.set({\'URL\':"'+url+'"},function(){ console.log("SET: "+"'+url+'");});'});
     }); 
     chrome.tabs.query({},function(tabs){
         console.log("dope"+url);
         var http = new XMLHttpRequest();
-        var siteURL = "https://review-digger.herokuapp.com/";
+        var siteURL = "http://localhost:5000/";
         var params = "amazonURL="+url;
         http.open("POST",siteURL,true);
 
